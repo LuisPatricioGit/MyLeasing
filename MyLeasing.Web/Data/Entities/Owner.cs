70cf48aa291984520c38c18cb754a207c66f3e73
LuisPatricioGit <luispatricio.info@gmail.com>
2023-06-01 11:02:01 +0100

    CRUD Owners

---
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Document*")]
+        public string Document { get; set; }
+
+        [Display(Name = "First Name*")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name*")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+
+        [Display(Name = "Cell Phone")]
+        public string CellPhone { get; set; }
+
+        public string Address { get; set; }
+
+        [Display(Name = "Owner Name")]
+        public string FullName 
+        {
+            get { return $"{FirstName} {LastName}"; } 
+        }
+    }
+}
